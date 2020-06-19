a75d0eab86e91eb5dfd3860a0c2b3d7001ef831e
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-19 14:15:00 +0100

    Added queries to select books. all, by genre, by id

--- database/ebooks_table.py
+++ database/ebooks_table.py
@@ -6,9 +6,56 @@ class DBEBooks(DBConnect):
     def __init__(self):
         super().__init__()
 
-    def insert_ebook(self, title, genre, release_date, user_id):
+    def insert_ebook(self, title, genre, release_date, user_id=None):
+        if user_id is None:
+            user_id = 1
         ins = self.ebooks.insert().values(title=title, genre=genre, release_date=release_date, user_id=user_id)
         result = self.conn.execute(ins)
         return result.inserted_primary_key
 
+    def select_all_books(self):
+        s = select([self.ebooks])
+        result_list = []
+        result = self.conn.execute(s)
+        while True:
+            row = result.fetchone()
+            if row is None:
+                break
+            result_list.append(row)
+        return result_list
 
+    def select_books_by_genre(self, genre):
+        result_list = []
+        s = select([self.ebooks]).\
+            where(and_(self.ebooks.c.genre == genre))
+        result = self.conn.execute(s)
+        while True:
+            row = result.fetchone()
+            if row is None:
+                break
+            result_list.append(row)
+        return result_list
+
+    def select_books_by_name(self, name):
+        result_list = []
+        s = select([self.ebooks]).\
+            where(and_(self.ebooks.c.title.like(name)))
+        result = self.conn.execute(s)
+        while True:
+            row = result.fetchone()
+            if row is None:
+                break
+            result_list.append(row)
+        return result_list
+
+    def select_one_book(self, book_id):
+        result_list = []
+        s = select([self.ebooks]).\
+            where(and_(self.ebooks.c.book_id == book_id))
+        result = self.conn.execute(s)
+        while True:
+            row = result.fetchone()
+            if row is None:
+                break
+            result_list.append(row)
+        return result_list
