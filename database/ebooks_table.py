3a6c0b0ed48380132e7b7dbec8286613b1971c9b
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-20 20:57:27 +0100

    index page now displays six random books every time it is loaded

--- database/ebooks_table.py
+++ database/ebooks_table.py
@@ -1,5 +1,6 @@
 from sqlalchemy import *
 from database.db_connect import DBConnect
+import random
 
 
 class DBEBooks(DBConnect):
@@ -10,7 +11,6 @@ class DBEBooks(DBConnect):
         if user_id is None:
             user_id = 1
         ins = self.ebooks.insert().values(title=title, author=author, genre=genre, release_date=release_date, image_source=image_source, description=description, user_id=user_id)
-        print(str(ins))
         result = self.conn.execute(ins)
         return result.inserted_primary_key
 
@@ -23,6 +23,9 @@ class DBEBooks(DBConnect):
             if row is None:
                 break
             result_list.append(row)
+        return result_list
+
+    def organise_list(self, result_list):
         title_list = []
         author_list = []
         genre_list = []
@@ -44,6 +47,26 @@ class DBEBooks(DBConnect):
         return_list.append(release_date_list)
         return_list.append(image_source_list)
         return_list.append(description_list)
+        return return_list
+
+    def select_all_separate(self):
+        result_list = self.select_all_books()
+        return_list = self.organise_list(result_list)
+        return return_list
+
+    def select_six_random(self):
+        result_list = self.select_all_books()
+        num_list = []
+        new_result_list = []
+        for x in range(6):
+            repeat = False
+            while not repeat:
+                rand = random.randint(0, len(result_list) - 1)
+                if rand not in num_list:
+                    repeat = True
+            num_list.append(rand)
+            new_result_list.append(result_list[rand])
+        return_list = self.organise_list(new_result_list)
         return return_list
 
     def select_books_by_genre(self, genre):
