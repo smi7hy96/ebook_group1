ac0275ef7462fdf0bd911749d5be7b548aa93193
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-21 22:12:09 +0100

    added user log in and password hashing

--- database/user_example.py
+++ database/user_example.py
@@ -1,4 +1,14 @@
 from database.users_table import *
 
+
+def example_user(user_new):
+    user_new.insert_user("Ryan", "ryan@example.com", "0715621616", "ryan1")
+    user_new.insert_user("Mergim", "mergim@example.com", "114165468541", "mergim1")
+    user_new.insert_user("Patrick", "patrick@example.com", "17847847", "patrick1")
+    user_new.insert_user("Avraj", "avraj@example.com", "0798452315", "avraj1")
+
+
 user = DBUsers()
-print(user.insert_user("Ryan", "example@example.com", "0715621616"))
+user.create_all_tables()
+example_user(user)
+print(user.log_in(1, "ryan1"))
